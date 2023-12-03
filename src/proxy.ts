8149a06e42358e051d2163b90a1e0547c27c2ab9
Matt Voget <matt.voget@gmail.com>
2023-12-03 13:22:40 -0700

    add cors

--- src/proxy.ts
+++ src/proxy.ts
@@ -4,33 +4,44 @@ import { ProxyRequest } from './types';
 
 export const proxy: HttpFunction = async (req, res) => {
 
-  const passedAuth = await googleAuthFilter(req, res);
+  res.set('Access-Control-Allow-Origin', '*');
 
-  if (!passedAuth) {
-    return;
+  if (req.method === 'OPTIONS') {
+    // Send response to OPTIONS requests
+    res.set('Access-Control-Allow-Methods', 'GET');
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
+    res.set('Access-Control-Max-Age', '3600');
+    res.status(204).send('');
+  } else {
+
+    const passedAuth = await googleAuthFilter(req, res);
+
+    if (!passedAuth) {
+      return;
+    }
+
+    const proxyRequest: ProxyRequest = req.body;
+
+    const requestConfig = {
+      url: proxyRequest.url,
+      method: proxyRequest.method.toString(),
+      data: proxyRequest.body,
+      headers: proxyRequest.headers
+    };
+
+    const client = axios.create();
+
+    let responseData = { status: "Failed", reason: '' };
+
+    try {
+      const response = await client.request(requestConfig);
+      responseData = response.data;
+    } catch (e) {
+      responseData.reason = e;
+    }
+
+    res.json(responseData);
   }
-
-  const proxyRequest: ProxyRequest = req.body;
-
-  const requestConfig = {
-    url: proxyRequest.url,
-    method: proxyRequest.method.toString(),
-    data: proxyRequest.body,
-    headers: proxyRequest.headers
-  };
-
-  const client = axios.create();
-
-  let responseData = { status: "Failed", reason: '' };
-
-  try {
-    const response = await client.request(requestConfig);
-    responseData = response.data;
-  } catch (e) {
-    responseData.reason = e;
-  }
-
-  res.json(responseData);
 };
 
 const googleAuthFilter = async (req, res): Promise<boolean> => {
